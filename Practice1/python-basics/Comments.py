e04f54e5713364f3f5d8390d0a964cd0b025f11e
sabi006 <sabinasir074@gmail.com>
2026-01-29 18:47:43 +0500

    Examples Commit

--- Practice1/python-basics/Comments.py
+++ Practice1/python-basics/Comments.py
@@ -6,3 +6,15 @@ print("Hello, World!") #This is a comment
 #ex3
 #print("Hello, World!")
 print("Cheers, Mate!")
+#ex4
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World!")
+#ex5
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
