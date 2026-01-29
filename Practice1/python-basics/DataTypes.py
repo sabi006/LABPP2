aab17d4df54160feb633e964360f78c859ae52e4
sabi006 <sabinasir074@gmail.com>
2026-01-29 13:41:09 +0500

    Examples Commit

--- Practice1/python-basics/DataTypes.py
+++ Practice1/python-basics/DataTypes.py
@@ -1,2 +1,35 @@
+#ex1
 x = 5
 print(type(x))
+#ex2
+x = "Hello World"
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+#ex3
+x = 1j
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+#ex4
+x = ("apple", "banana", "cherry")
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+#ex5
+x = {"name" : "John", "age" : 36}
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
