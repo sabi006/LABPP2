f6a087719f06583d01b7549dc531f640d692bec7
sabi006 <sabinasir074@gmail.com>
2026-01-29 13:44:34 +0500

    Examples Commit

--- Practice1/python-basics/Strings.py
+++ Practice1/python-basics/Strings.py
@@ -1,2 +1,22 @@
+#ex1
 print("Hello")
 print('Hello')
+#ex2
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+#ex3
+a = "Hello"
+print(a)
+#ex4
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+#ex5
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a)
