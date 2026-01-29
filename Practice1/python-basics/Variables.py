5a774d921f06bda3d4c25d6c34c1f2cc948f63a5
sabi006 <sabinasir074@gmail.com>
2026-01-29 13:47:14 +0500

    Examples Commit

--- Practice1/python-basics/Variables.py
+++ Practice1/python-basics/Variables.py
@@ -1,4 +1,22 @@
+#ex1
 x = 5
 y = "John"
 print(x)
 print(y)
+#ex2
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+#ex3
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+#ex4
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+#ex5
+x = "John"
+# is the same as
+x = 'John'
