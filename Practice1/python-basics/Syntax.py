0d5803d9649cd9732f34706ea990c6ec7b4d5b24
sabi006 <sabinasir074@gmail.com>
2026-01-29 13:46:11 +0500

    Examples Commit

--- Practice1/python-basics/Syntax.py
+++ Practice1/python-basics/Syntax.py
@@ -1,2 +1,14 @@
+#ex1
 if 5 > 2:
   print("Five is greater than two!")
+#ex2
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!") 
+#ex3
+x = 5
+y = "Hello, World!"
+#ex4
+#This is a comment.
+print("Hello, World!")
