c13e393a1658308340f776d30ee23ab566a35caf
sabi006 <sabinasir074@gmail.com>
2026-02-02 01:42:57 +0500

    Example commit

---
+++ Practice2/if_else/short_hand_if.py
@@ -0,0 +1,22 @@
+#ex1
+a = 5
+b = 2
+if a > b: print("a is greater than b")
+#ex2
+a = 2
+b = 330
+print("A") if a > b else print("B")
+#ex3
+a = 10
+b = 20
+bigger = a if a > b else b
+print("Bigger is", bigger)
+#ex4
+a = 330
+b = 330
+print("A") if a > b else print("=") if a == b else print("B")
+#ex5
+x = 15
+y = 20
+max_value = x if x > y else y
+print("Maximum value:", max_value)
