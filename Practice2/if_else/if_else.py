e9dc755481082b2bd6df44115d6093db8e2b1aa1
sabi006 <sabinasir074@gmail.com>
2026-02-02 12:04:55 +0500

    Example commit

--- Practice2/if_else/if_else.py
+++ Practice2/if_else/if_else.py
@@ -1,38 +1,19 @@
+
 #ex1
-a = 200
-b = 33
-if b > a:
-  print("b is greater than a")
-elif a == b:
-  print("a and b are equal")
-else:
-  print("a is greater than b")
-#ex2
 a = 200
 b = 33
 if b > a:
   print("b is greater than a")
 else:
   print("b is not greater than a")
-#ex3
+#ex2
 number = 7
 
 if number % 2 == 0:
   print("The number is even")
 else:
   print("The number is odd")
-#ex4
-temperature = 22
-
-if temperature > 30:
-  print("It's hot outside!")
-elif temperature > 20:
-  print("It's warm outside")
-elif temperature > 10:
-  print("It's cool outside")
-else:
-  print("It's cold outside!")
-#ex5
+#ex3
 username = "Emil"
 
 if len(username) > 0:
---
+++ Practice2/if_else/switch.py
@@ -0,0 +1,20 @@
+#ex1
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
+#ex2
+temperature = 22
+
+if temperature > 30:
+  print("It's hot outside!")
+elif temperature > 20:
+  print("It's warm outside")
+elif temperature > 10:
+  print("It's cool outside")
+else:
+  print("It's cold outside!")
