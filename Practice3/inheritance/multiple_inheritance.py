7e8586c05232da751701baa566b394f505b1042b
sabi006 <sabinasir074@gmail.com>
2026-02-14 00:39:40 +0500

    commit ex

---
+++ Practice3/inheritance/multiple_inheritance.py
@@ -0,0 +1,59 @@
+#ex1
+class A:
+    def hello(self):
+        print("Hello from A")
+
+class B:
+    def hi(self):
+        print("Hi from B")
+
+class C(A, B):
+    pass
+
+c = C()
+c.hello()
+c.hi()
+
+
+print("------")
+
+#ex2
+class A:
+    def greet(self):
+        print("A greet")
+
+class B:
+    def greet(self):
+        print("B greet")
+
+class C(A, B):
+    pass
+
+c = C()
+c.greet()   # A greet
+
+
+print("------")
+
+#ex3
+print(C.__mro__)
+
+
+print("------")
+
+#ex4
+class A:
+    def greet(self):
+        print("Hello from A")
+
+class B:
+    def greet(self):
+        print("Hello from B")
+
+class C(A, B):
+    def greet(self):
+        print("Hello from C")
+
+c = C()
+c.greet()
+
