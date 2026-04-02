d0a53eca14bc01d81586cbdde31ab55f21401521
sabi006 <sabinasir074@gmail.com>
2026-04-02 15:31:43 +0500

    Add Practice8 - PhoneBook with functions and stored procedures

--- Practice8/connect.py
+++ Practice8/connect.py
@@ -5,7 +5,7 @@ DB_CONFIG = {
     "database": "phonebook_db",
     "user": "postgres",
     "password": "23455432",
-    "port": 5432  # добавил порт, если используется стандартный
+    "port": 5432  
 }
 
 def connect():
