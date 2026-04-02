bee687c0da1eee4961bf4e812948507c397b2013
sabi006 <sabinasir074@gmail.com>
2026-04-02 15:27:13 +0500

    add 8

---
+++ Practice8/connect.py
@@ -0,0 +1,24 @@
+import psycopg2
+
+DB_CONFIG = {
+    "host": "localhost",
+    "database": "phonebook_db",
+    "user": "postgres",
+    "password": "23455432",
+    "port": 5432  # добавил порт, если используется стандартный
+}
+
+def connect():
+    try:
+        conn = psycopg2.connect(
+            host=DB_CONFIG["host"],
+            database=DB_CONFIG["database"],
+            user=DB_CONFIG["user"],
+            password=DB_CONFIG["password"],
+            port=DB_CONFIG["port"]
+        )
+        print("Connected successfully")
+        return conn
+    except Exception as e:
+        print("Connection error:", e)
+        return None
